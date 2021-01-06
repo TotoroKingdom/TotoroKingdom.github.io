d2d420d646931c435bd6043faf7055c6452c8339
TotoroKingdom <634581304@qq.com>
2021-01-06 11:08:46 +0800

    Site updated: 2021-01-06 11:08:45

---
+++ 2020/07/01/07-01/test.js
@@ -0,0 +1,13 @@
+/* //输入框
+prompt('如果我是DJ，你会爱我吗？');
+//输出框
+alert('If I was DJ,Do you love me?');
+//控制台输出，测试用
+console.log("？？？？？？"); */
+
+var age;
+age = 18;
+console.log(age);
+var name;
+name = ('ooo');
+prompt(name);
